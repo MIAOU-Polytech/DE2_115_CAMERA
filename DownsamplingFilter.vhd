7d146c3852c629fd837f2d9e243574b2f7679ccb
piernov <piernov@piernov.org>
2017-07-21 15:10:27 +0200

    Update downsampling

--- DownsamplingFilter.vhd
+++ DownsamplingFilter.vhd
@@ -30,10 +30,10 @@ ARCHITECTURE Behavior OF DownsamplingFilter IS
 
 	signal CoordX : unsigned(10 DOWNTO 0);
 	signal CoordY : unsigned(10 DOWNTO 0);
-	constant SIZE : unsigned := to_unsigned(29, 5);
+	constant SIZE : unsigned := to_unsigned(28, 5);
 
-	TYPE tab_samples is array( 0 to 28 ) of STD_LOGIC_VECTOR(11 DOWNTO 0);
-	SIGNAL tmp_samples : tab_samples ; 
+	TYPE tab_samples is array( 0 to 783 ) of STD_LOGIC_VECTOR(11 DOWNTO 0) ;
+	SIGNAL tmp_samples : tab_samples := (others => "000000000000"); 
 
 	
 	component line_buf is 
@@ -57,51 +57,64 @@ ligne2:line_buf port map (clk, reset, '1', pixvalid, tmp_pix(4), tmp_pix(5)) ;
 CoordX <= unsigned(iX_Cont);
 CoordY <= unsigned(iY_Cont);
 
-process (clk, reset, pixvalid) begin
-	if(clk'Event and clk = '1' and pixvalid = '1') then
-		tmp_pix(0) <= "0000" & pixin; 
+--process (clk, reset, pixvalid) begin
+--	if(clk'Event and clk = '1' and pixvalid = '1') then
+--		tmp_pix(0) <= "0000" & pixin; 
+--		tmp_pix(1) <= tmp_pix(0) ; 
+--		tmp_pix(3) <= tmp_pix(2) ; 
+--		tmp_pix(4) <= tmp_pix(3) ;
+--		tmp_pix(6) <= tmp_pix(5) ; 
+--		tmp_pix(7) <= tmp_pix(6) ;
+--	end if ; 
+--end process ; 
+
+
+
+process (clk, enable, pixvalid) begin 
+	if (rising_edge(clk)) then
+		if (pixvalid = '1') then
+		
+		
+			tmp_pix(0) <= "0000" & pixin; 
 		tmp_pix(1) <= tmp_pix(0) ; 
 		tmp_pix(3) <= tmp_pix(2) ; 
 		tmp_pix(4) <= tmp_pix(3) ;
 		tmp_pix(6) <= tmp_pix(5) ; 
 		tmp_pix(7) <= tmp_pix(6) ;
-	end if ; 
-end process ; 
-
-
-
-process (clk, enable, pixvalid) begin 
-	if (enable = '1' and pixvalid = '1') then
-		if (CoordX > 200 and CoordY > 200) then 
+		
+		if (enable = '1' and CoordX >= 200 and CoordY >= 200) then 
 			if (CoordX < (3*SIZE + 200) and CoordY < (3*SIZE + 200)) then
-				--if (((CoordX - 1) / 3) * 3 = (CoordX - 1) and ((CoordY - 1) / 3) * 3 = (CoordY -1 )) then
-				if ((CoordX mod 3) = 1 and (CoordY mod 3) = 1) then
+				if (((CoordX - 200) mod 3) = 1 and ((CoordY - 200) mod 3) = 1) then
 					-- mean filtre
-					tmp2 <= (signed(tmp_pix(7)) + signed(tmp_pix(6)) + signed(tmp_pix(5)) + signed(tmp_pix(4)) + signed(tmp_pix(3)) + signed(tmp_pix(2)) + signed(tmp_pix(1)) + signed(tmp_pix(0)) + ("000" & signed(pixin))) / 9;
+					tmp2 <= (signed(tmp_pix(7)) + signed(tmp_pix(6)) + signed(tmp_pix(5)) + signed(tmp_pix(4)) + signed(tmp_pix(3)) + signed(tmp_pix(2)) + signed(tmp_pix(1)) + signed(tmp_pix(0)) + signed("0000" & unsigned(pixin))) / 9;
 					tmp_samples(samplen) <= std_logic_vector(tmp2)(11 DOWNTO 0);
 					samplen <= samplen + 1;
-					tmp <= "000000000000";
+					tmp <= std_logic_vector(tmp2)(11 DOWNTO 0);
 				else
 					tmp <= "110000000000";
 				end if;
-			elsif (CoordX < (4*SIZE + 200) and CoordY < (4*SIZE + 200)) then
-				--tmp <= tmp_samples(cnt);
+			elsif (CoordX < (4*SIZE + 200) and CoordY < (4*SIZE + 200) and CoordX >= (3*SIZE + 200) and CoordY >= (3*SIZE + 200)) then
+				--cnt <= 1;
+				tmp <= tmp_samples(cnt);
 				cnt <= cnt + 1;
-				samplen <= 0;
-				tmp <= "111111111111";
-			elsif (CoordX > (4*SIZE + 200) and CoordY > (4*SIZE +200)) then
+			elsif (CoordX >= (4*SIZE + 200) and CoordY >= (4*SIZE +200)) then
 				tmp <= pixin;
 				cnt <= 0;
 				samplen <= 0;
 			else
 				tmp <= pixin;
 			end if;
+		elsif (CoordX < 200 and CoordY < 200) then
+			cnt <= 0;
+			samplen <= 0;
+			tmp <= pixin;
 		else
 			tmp <= pixin;
 		end if;
 	else
 		tmp <= pixin;
 	end if ; 
+	end if;
 end process ; 
 pixout <= tmp; 
 END Behavior;
